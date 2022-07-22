31c66fadf8e18b12b0f86f7be9c4767e1b23419b
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-07-22 13:32:49 +0200

    Finish first version of Extract

---
+++ src/Extract.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conia\I18n;
+
+use Conia\Cli\Command;
+
+class Extract extends Command
+{
+    use Validates;
+
+    protected string $name = 'extract';
+    protected string $group = 'Internationalization/gettext';
+    protected string $prefix = 'i18n';
+    protected string $description = 'Extracts translatable strings from source files';
+
+    public function __construct(
+        protected string $dir,
+        protected string $domain,
+        protected readonly array $sources,
+    ) {
+    }
+
+    protected function extract(
+        string $srcdir,
+        string $glob,
+        string $type,
+        string $domain,
+        string $potfile,
+        bool $join,
+    ): void {
+        $find =  " -type d \\( " .
+            "-name node_modules -o -name Plugin " . // excluded
+            "\\) -prune -false -o -name";
+
+        $cmd = "find $srcdir" . $find . " '$glob' | xargs " .
+            'xgettext' .
+            ' --from-code=UTF-8' .
+            ($join ? ' --join-existing' : '') .
+            " --language=$type" .
+            " --default-domain=$domain" .
+            " --output=$potfile";
+
+        passthru($cmd);
+    }
+
+    public function run(): int
+    {
+        $this->checkXgettext();
+        $this->validateDir($this->dir);
+        $this->validateDomain($this->domain);
+
+        $joinExisting = false;
+        $potfile = $this->dir . '/' . $this->domain . '.pot';
+
+        if (is_file($potfile)) {
+            $joinExisting = true;
+        }
+
+        foreach ($this->sources as $source) {
+            echo 'Extracting ' . $source->glob . ' from ' . $source->dir . "\n";
+            $this->validateDir($source->dir);
+
+            $this->extract(
+                $source->dir,
+                $source->glob,
+                $source->type,
+                $this->domain,
+                $potfile,
+                $joinExisting,
+            );
+
+            $joinExisting = true;
+        }
+
+        return 0;
+    }
+}
---
+++ src/Source.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conia\I18n;
+
+class Source
+{
+    public function __construct(
+        public readonly string $dir,
+        public readonly string $glob,
+        public readonly string $type
+    ) {
+    }
+}
